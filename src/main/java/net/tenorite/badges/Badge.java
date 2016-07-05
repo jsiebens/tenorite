b88339b871eaa9de255bafd33c7dbcf07112b0f5
Johan Siebens <johan.siebens@gmail.com>
2016-07-05 13:35:07 +0200

    add title and description to Badge, add an overview page of badges

--- src/main/java/net/tenorite/badges/Badge.java
+++ src/main/java/net/tenorite/badges/Badge.java
@@ -4,6 +4,12 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import net.tenorite.game.GameModeId;
 import net.tenorite.util.ImmutableStyle;
 import org.immutables.value.Value;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Value.Immutable
 @ImmutableStyle
@@ -22,4 +28,28 @@ public abstract class Badge {
 
     public abstract BadgeType getBadgeType();
 
+    @Value.Lazy
+    public String getTitle() {
+        String key = getBadgeType() + ".title";
+        return properties(getGameModeId()).getProperty(key, key);
+    }
+
+    @Value.Lazy
+    public String getDescription() {
+        String key = getBadgeType() + ".description";
+        return properties(getGameModeId()).getProperty(key, key);
+    }
+
+    private static final Map<GameModeId, Properties> PROPERTIES = new ConcurrentHashMap<>();
+
+    private static Properties properties(GameModeId gameModeId) {
+        return PROPERTIES.computeIfAbsent(gameModeId, g -> {
+            try {
+                return PropertiesLoaderUtils.loadAllProperties("" + g + "_badges.properties");
+            }
+            catch (IOException e) {
+                return new Properties();
+            }
+        });
+    }
 }
--- src/main/java/net/tenorite/web/TempoController.java
+++ src/main/java/net/tenorite/web/TempoController.java
@@ -1,6 +1,9 @@
 package net.tenorite.web;
 
+import net.tenorite.badges.Badge;
+import net.tenorite.badges.BadgeValidator;
 import net.tenorite.core.Tempo;
+import net.tenorite.game.GameMode;
 import net.tenorite.game.GameModeId;
 import net.tenorite.game.GameModes;
 import net.tenorite.winlist.WinlistItem;
@@ -13,21 +16,23 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 @Controller
-public class WinlistController {
+public class TempoController {
 
     private GameModes gameModes;
 
     private WinlistRepository winlistRepository;
 
     @Autowired
-    public WinlistController(GameModes gameModes, WinlistRepository winlistRepository) {
+    public TempoController(GameModes gameModes, WinlistRepository winlistRepository) {
         this.gameModes = gameModes;
         this.winlistRepository = winlistRepository;
     }
 
-    @RequestMapping("/t/{tempo}/winlist/{mode}")
-    public ModelAndView recentGames(@PathVariable("tempo") Tempo tempo, @PathVariable("mode") String gameMode) {
+    @RequestMapping("/t/{tempo}/m/{mode}/winlist")
+    public ModelAndView winlist(@PathVariable("tempo") Tempo tempo, @PathVariable("mode") String gameMode) {
         GameModeId id = GameModeId.of(gameMode);
 
         List<WinlistItem> winlistItems = winlistRepository.winlistOps(tempo).loadWinlist(id);
@@ -38,6 +43,20 @@ public class WinlistController {
                 .addObject("gameModes", gameModes)
                 .addObject("gameMode", gameModes.get(id))
                 .addObject("winlist", winlistItems);
+    }
+
+    @RequestMapping("/t/{tempo}/m/{mode}/badges")
+    public ModelAndView badges(@PathVariable("tempo") Tempo tempo, @PathVariable("mode") String gameModeId) {
+        GameMode gameMode = gameModes.get(GameModeId.of(gameModeId));
+
+        List<Badge> badges = gameMode.getBadgeValidators().stream().map(BadgeValidator::getType).collect(toList());
+
+        return
+            new ModelAndView("badges")
+                .addObject("tempo", tempo)
+                .addObject("gameModes", gameModes)
+                .addObject("gameMode", gameMode)
+                .addObject("badges", badges);
     }
 
 }
