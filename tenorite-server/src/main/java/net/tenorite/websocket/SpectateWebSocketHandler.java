30951d1294f68c713371ddc00191a87c5842dc8e
Johan Siebens <johan.siebens@gmail.com>
2016-09-30 09:35:44 +0200

    add missing header

--- tenorite-server/src/main/java/net/tenorite/websocket/SpectateWebSocketHandler.java
+++ tenorite-server/src/main/java/net/tenorite/websocket/SpectateWebSocketHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.tenorite.websocket;
 
 import akka.actor.ActorRef;
@@ -22,6 +37,9 @@ import static akka.actor.ActorRef.noSender;
 import static akka.actor.PoisonPill.getInstance;
 import static java.util.Optional.ofNullable;
 
+/**
+ * @author Johan Siebens
+ */
 public class SpectateWebSocketHandler extends TextWebSocketHandler {
 
     private final ObjectMapper mapper = new ObjectMapper();
