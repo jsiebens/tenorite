db60aa2c0c80934034619262e77814e598010753
Johan Siebens <johan.siebens@gmail.com>
2016-09-30 09:54:17 +0200

    upgrade to Spring Boot 1.4.1

--- tenorite-server/src/test/java/net/tenorite/AbstractTenoriteServerTestCase.java
+++ tenorite-server/src/test/java/net/tenorite/AbstractTenoriteServerTestCase.java
@@ -23,7 +23,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Johan Siebens
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = TenoriteServer.class)
+@SpringBootTest(classes = TenoriteServer.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractTenoriteServerTestCase {
 
 }
